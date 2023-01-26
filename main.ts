537c05ce0d271b4a4a4f613af881db28ad1bf61d
skrevolve <sukyu0919@naver.com>
2023-01-26 15:52:05 +0900

    fix

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ import { S3Client } from '@aws-sdk/client-s3'
 
 AWS.config.loadFromPath(__dirname + '/src/config/aws_config.json')
 
-//=======================================================================
+// send email by aws mail template
 const mailTemplate = new MailTemplate(new AWS.SES())
 
 const mailTemplateResult = await mailTemplate.sendBulkTemplateEmail([
@@ -14,20 +14,13 @@ const mailTemplateResult = await mailTemplate.sendBulkTemplateEmail([
     'test2@gmail.com',
 ]);
 if (!mailTemplateResult) console.error('mail template send error')
-//=======================================================================
 
-
-
-//=======================================================================
+// send email by nodemailer
 const mail = new Mail(new AWS.SES({ apiVersion: '2010-12-01' }))
 
 const mailResult = await mail.send('test@gmail.com')
 if (!mailResult) console.error('mail send err')
-//=======================================================================
 
-
-
-//=======================================================================
 const image = new MulterS3(
     new S3Client({
         region: 'YOUR_S3_REGION',
@@ -39,9 +32,10 @@ const image = new MulterS3(
     'YOUR_S3_BUCKET'
 )
 
+// upload single image
 const uploadResult = await image.uploadSingle('fileDir', 'imgName')
 if (!uploadResult) console.error('upload image error')
 
+// delete image
 const deleteResult = await image.delete('fileDir', 'fileName')
 if (!deleteResult) console.error('delete image error')
-//=======================================================================
